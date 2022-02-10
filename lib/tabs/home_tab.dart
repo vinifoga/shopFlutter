67b76c8d9dd15a76f9fe35e616456d6f952acaaa
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-02-10 05:58:26 -0300

    Add Firebase Photos to Home

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Widget _buildBodyBack() =>
-        Container(
+    Widget _buildBodyBack() => Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color(0xff000428),
-                  Color(0xff004e92),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight
-            ),
+            gradient: LinearGradient(colors: [
+              Color(0xff000428),
+              Color(0xff004e92),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
         );
 
@@ -26,7 +22,7 @@ class HomeTab extends StatelessWidget {
         _buildBodyBack(),
         CustomScrollView(
           slivers: [
-            SliverAppBar(
+            const SliverAppBar(
               floating: true,
               snap: true,
               backgroundColor: Colors.transparent,
@@ -37,8 +33,10 @@ class HomeTab extends StatelessWidget {
               ),
             ),
             FutureBuilder<QuerySnapshot>(
-              future: FirebaseFirestore.instance.collection("home").orderBy(
-                  "pos").get(),
+              future: FirebaseFirestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .get(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return SliverToBoxAdapter(
@@ -46,25 +44,28 @@ class HomeTab extends StatelessWidget {
                       height: 200,
                       alignment: Alignment.center,
                       child: const CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.white),),
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
                     ),
                   );
                 } else {
-                  return   StaggeredGrid.count(
+                  return SliverStaggeredGrid.count(
                     crossAxisCount: 2,
                     mainAxisSpacing: 1.0,
                     crossAxisSpacing: 1.0,
-                    children: [
-                      // snapshot.data.docs.map((doc) => StaggeredGridTile.count(
-                      //     crossAxisCellCount: doc.data(["x"]),
-                      //     mainAxisCellCount: doc.get(["y"]),
-                      //     child: Tile(index: doc.get("pos"))))
-                    ],
+                    staggeredTiles: snapshot.data!.docs.map((doc) {
+                      return StaggeredTile.count(doc.get('x'), doc.get('y'));
+                    }).toList(),
+                    children: snapshot.data!.docs.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.get('image'),
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
-
             ),
           ],
         )
